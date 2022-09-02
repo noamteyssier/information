f087b20020401d45af8ae7ae2c30ec512afc0eb5
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-02 15:30:27 -0700

    added splash to docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+//! # Information
+//! This is a crate to perform [information theory](https://en.wikipedia.org/wiki/Information_theory) calculations using [`ndarray`] arrays.
+//!
+//! ## Entropy Functions
+//! * [`entropy()`]
+//! * [`joint_entropy!()`]
+//! * [`conditional_entropy()`]
+//!
+//! ## Information Functions
+//! * [`mutual_information()`]
+//! * [`conditional_mutual_information()`]
+//!
+//! ## Utility
+//! ### `N-d` Histogram
+//! * [`hist1d`]
+//! * [`hist2d`]
+//! * [`hist3d`]
+//!
+//! ### `N-d` Probability
+//! * [`prob1d`]
+//! * [`prob2d`]
+//! * [`prob3d`]
+//!
 pub mod hist;
 pub mod prob;
 pub mod entropy;
