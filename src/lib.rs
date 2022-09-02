26421aaa2cedcb3e9bce5868e35d74a6cd73f214
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-02 11:00:07 -0700

    expose functions to api

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,11 @@ pub mod joint;
 pub mod conditional;
 pub mod mutual;
 pub mod cmi;
+
+pub use hist::{hist1d, hist2d, hist3d};
+pub use prob::{prob1d, prob2d, prob3d};
+pub use entropy::entropy;
+pub use joint::{joint_entropy, joint_entropy_3d};
+pub use conditional::conditional_entropy;
+pub use mutual::mutual_information;
+pub use cmi::conditional_mutual_information;
